3ad198e25ef8c6e428b5f57de8d08c737e014863
codedave <208051940+codedave15@users.noreply.github.com>
2025-06-16 14:41:59 +0800

    chore: configure client-server integration for deployment
    - Served client and server from a unified path for production
    - Updated Axios baseURL to support dev and prod environments
    - Adjusted BASE_URL in useAuthStore to align with deployment setup

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,11 @@ import connectDB from './lib/connectDB.js';
 import { app, server } from './lib/socket.js';
 import authRoutes from './routes/authRoute.js';
 import messageRoutes from './routes/messageRoute.js';
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || "http://localhost:5001";
+const __dirname = path.resolve();
 
 app.use(cors(
   {
@@ -22,6 +24,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
