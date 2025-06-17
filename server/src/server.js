8a95a0a8bd38f6004f84e36f7897cc5f06a00bee
codedave <208051940+codedave15@users.noreply.github.com>
2025-06-17 18:00:38 +0800

    fix: update production static file path to match Render's directory structure

--- server/src/server.js
+++ server/src/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get(/^\/(?!api).*/, (_, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
